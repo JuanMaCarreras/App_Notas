00b127d908b9c94a2317bfed4bcec8551b439b84
JuanMaCarrearas <manuucarreras@gmail.com>
2023-09-03 18:45:06 -0300

    handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const path = require('path')
 
 //initializations
@@ -7,14 +8,24 @@ const app = express()
 //Settigs
 app.set('port', process.env.port || 3000)
 app.set('views', path.join(__dirname, 'views'))
+app.engine(
+    '.hbs',
+    exphbs.engine({
+        extname: 'hbs',
+        defaultLayout: false,
+        layoutsDir: 'views/layouts/',
+        patialsDir: path.join(app.get('views'), 'partials'),
+    }),
+)
 
+app.set('view engine', '.hbs')
 //Middlewares
 
 //Global variables
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('Hola Mundo!!')
+    res.render('index')
 })
 
 //Static files
