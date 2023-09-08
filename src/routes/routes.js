e2d9b830af7afbdabae3ab72bb59016f535bfc62
JuanMaCarrearas <manuucarreras@gmail.com>
2023-09-08 18:57:07 -0300

    add post method

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,24 @@
 import { Router } from 'express'
+import Note from '../models/notes'
 
 const router = Router()
 
 router.get('/', (req, res) => {
     res.render('index')
+})
+
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
+router.post('/note/add', (req, res) => {
+    // const { name, description } = req.body
+
+    // const note = Note(req.body)
+
+    // console.log(note)
+
+    res.send('todo OK')
 })
 
 export default router
